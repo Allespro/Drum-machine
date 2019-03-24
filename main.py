5b75a4c5318e71dc4c044fe489903942388f4258
Allespro <arcy.all@yahoo.com>
2019-03-24 17:38:42 +0000

    Mulisound Version
    Play more music!

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
+from threading import Thread
 from playsound import playsound
 from pynput import keyboard
 
 Samples = {}
 SamFile = "list.txt"
 
-
 def on_press(key):
     try:
         if(key.char in Samples):
-            playsound(Samples[key.char][0].replace("\n", ""))
+            thread1 = Thread(target=playsound, args=(Samples[key.char][0].replace("\n", ""),))
+            thread1.start()
         else:
             pass
-        #print('alphanumeric key {0} pressed'.format(
-        #    key.char))
     except AttributeError:
         pass
+
 def on_release(key):
-    #print('{0} released'.format(key))
     if key == keyboard.Key.esc:
-        # Stop listener
         return False
 
 def preload():
@@ -34,7 +32,6 @@ def board():
 			on_release=on_release) as listener:
 		listener.join()
 
-
 if __name__ == '__main__':
-	preload()
-	board()
+    preload()
+    board()
